0e132eadc004e2de38e0f2d02849ab1c0887d34a
prasannad357 <59197308+prasannad357@users.noreply.github.com>
2020-05-05 23:18:40 +0530

    Create Notes.kt

---
+++ app/java/com.myprojects.mynotes/Notes.kt
@@ -0,0 +1,13 @@
+package com.awesome.mystartup
+
+class Notes{
+    var nodeId:Int? = null
+    var nodeTitle:String? = null
+    var nodeDetails:String? = null
+
+    constructor(nodeId:Int,nodeTitle:String,nodeDetails:String){
+        this.nodeId = nodeId
+        this.nodeTitle = nodeTitle
+        this.nodeDetails = nodeDetails
+    }
+}
